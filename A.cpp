0acdef76ea39fd168ce0a82b78cea341b958c16e
Manish <manishsahu6961@gmail.com>
2023-04-16 23:44:45 +0530

    Added all files required for competitive programming

---
+++ A.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+#ifndef ONLINE_JUDGE
+#include "algo/debug.h"
+#else
+#define debug(...) (void)0
+#endif
+#define all(v) v.begin(),v.end()
+#define int long long int
+#define IMAX INT64_MAX
+#define IMIN INT64_MIN
+#define endl "\n"
+const int MOD =(int)1000000000+7;
+using namespace std;
+void cases();
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(0);
+    clock_t z = clock();
+#ifndef ONLINE_JUDGE
+    freopen("input.txt" , "r", stdin );
+    freopen("output.txt", "w", stdout);
+    freopen("error.txt" , "w", stderr);
+#endif
+    int T = 1;
+    cin >> T;
+    while (T--)
+    {
+        cases();
+#ifndef ONLINE_JUDGE
+cout << "____________________________________\n";
+#endif
+    }
+    cerr << "Run time : " << ((double)(clock() - z) / CLOCKS_PER_SEC);
+}
+void cases()
+{
+    int n,cnt=0;
+    cin>>n;
+    
+    return;
+}
